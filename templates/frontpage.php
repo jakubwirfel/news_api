23c75782b999c3ce9d2377ab43ffb586e600a593
jakubwirfel <jakubwirfel@gmail.com>
2020-08-28 00:27:03 +0200

    Create view fot user news

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -8,7 +8,13 @@
                     <div class="content">
                         <h2>Testowy news 1</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident fugiat reprehenderit asperiores iste ipsa perspiciatis, ea aspernatur alias corrupti vel inventore nihil neque sit laudantium quae, tempora voluptates rem culpa!</p>
-                        <span>Jakub Wirfel / 27.08.2020</span>
+                        <div class="box">
+                            <span>Jakub Wirfel / 27.08.2020</span>
+                            <div class="views">
+                                <i class="fas fa-eye"></i>
+                                <span>300</span>
+                            </div>
+                        </div>
                     </div>
                     <div class="img-box">
                         <img src="https://via.placeholder.com/300.png/09f/fffC/O https://placeholder.com/" alt="test">
@@ -20,9 +26,9 @@
         <aside class="info">
             <div class="best-authors">
                 <h3>Best authors of the week</h3>
-                <p><span>1.</span> Test 1</p>
-                <p><span>2.</span> Test 2</p>
-                <p><span>3.</span> Test 3</p>
+                <a href="profile.php"><span>1.</span> Test 1</a>
+                <a href="profile.php"><span>2.</span> Test 2</a>
+                <a href="profile.php"><span>3.</span> Test 3</a>
             </div>
             <div class="best-news">
                 <h3>Best news of the week</h3>
---
+++ templates/user_profile.php
@@ -0,0 +1,45 @@
+<?php include 'inc/header.php';?>
+<?php include 'inc/navbar.php';?>
+    <main class="user-container">
+        <section class="user-data">
+            <h2>User Test news</h2>
+            <div class="box">
+                <input disabled type="text" placeholder="test@test.wp">
+                <a href="#change">Change</a>
+            </div>
+            <div class="box">
+                <input disabled type="password" value="emptyemptyempty">
+                <a href="#change">Change</a>
+            </div>
+        </section>
+        <article class="user-news">
+            <div class="content">
+                <h2>Testowy news 1</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident fugiat reprehenderit asperiores iste ipsa perspiciatis, ea aspernatur alias corrupti vel inventore nihil neque sit laudantium quae, tempora voluptates rem culpa!</p>
+                <div class="box">
+                    <span>Jakub Wirfel / 27.08.2020</span>
+                    <div class="views">
+                        <i class="fas fa-eye"></i>
+                        <span>300</span>
+                    </div>
+                </div>
+            </div>
+            <div class="img-box">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <img src="https://via.placeholder.com/300.png/09f/fffC/O https://placeholder.com/" alt="test">
+            </div>
+        </article>
+    </main>
+<script>
+    window.onscroll = () => {
+        const nav = document.querySelector('#navbar');
+        if (this.scrollY <= 1) {
+            nav.classList.add("navigation");
+            nav.classList.remove("scroll");
+        }
+        else {
+            nav.classList.add("scroll", "navigation");
+        }
+    };
+</script>
+<?php include 'inc/footer.php';?>

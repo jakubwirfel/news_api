37cfb72c8598f3cac3f1a813bd53f969d5aaaa51
jakubwirfel <jakubwirfel@gmail.com>
2020-08-27 21:40:17 +0200

    Main page CSS view created

--- templates/inc/footer.php
+++ templates/inc/footer.php
@@ -1,2 +1,6 @@
+<footer>
+    <h1>Created by: Jakub wirfel</h1>
+    <?php displayMessage();?>
+</footer>
 </body>
 </html>
---
+++ templates/inc/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navigation">
+    <div class="nav-box" id="navbar">
+        <div class="logo">
+            <h1><?php echo SITE_TITLE ?></h1>
+        </div>
+        <div class="items">
+            <a class="item">
+                Home
+            </a>
+            <a class="item">
+                Sign In
+            </a>
+            <a class="item">
+                Sign Up
+            </a>
+        </div>
+    </div>
+</nav>
